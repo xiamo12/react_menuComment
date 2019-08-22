8f9c29964077e6a3affaf503ceb3a48d651f7fe2
xiamo12 <364023396@qq.com>
2019-08-22 14:31:46 +0800

    订单评价界面

---
+++ build/precache-manifest.b4c3a3e5c30ef9af4b86306e35f0d39b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9483ba6a5f0a800f7cfe87afa8c24113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3725678b8345fabd9ddc",
+    "url": "/static/css/main.94127109.chunk.css"
+  },
+  {
+    "revision": "6f6c0f29478d92308e66",
+    "url": "/static/js/2.c6481815.chunk.js"
+  },
+  {
+    "revision": "3725678b8345fabd9ddc",
+    "url": "/static/js/main.a5d1c813.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
